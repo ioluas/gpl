9c18667567980849500d888a11aa8d0d87ef51a0
Jad Haboush <jad.haboush@idgateway.co.uk>
2022-08-16 15:02:28 +0100

    Add channels option to fetch command

--- README.md
+++ README.md
@@ -52,3 +52,47 @@ This outputs a file `lissajous.gif` in current directory.
 ### Command fetch
 
 Simple command to fetch given urls concurrently, reports status code and content length. Follows redirects keeping cookies
+
+```shell
+# Using wait group to manage go routines
+./gpl fetch --url https://google.com --url https://github.com --url https://slack.com
+URLs to fetch: [https://google.com https://github.com https://slack.com]
+---------------
+Url:    https://github.com
+StatusCode:     200
+Length: 300.2kB
+Fetch time:     0.246s
+---------------
+Url:    https://slack.com
+StatusCode:     200
+Length: 96.65kB
+Fetch time:     0.313s
+---------------
+Url:    https://google.com
+StatusCode:     200
+Length: 13.45kB
+Fetch time:     0.371s
+---------------
+Command time: 0.371s
+
+# Using channels to manage go routines
+./gpl fetch --use-channels --url https://google.com --url https://github.com --url https://slack.com
+URLs to fetch: [https://google.com https://github.com https://slack.com]
+---------------
+Url:    https://github.com
+StatusCode:     200
+Length: 300.2kB
+Fetch timei:    0.237s
+---------------
+Url:    https://google.com
+StatusCode:     200
+Length: 13.45kB
+Fetch timei:    0.335s
+---------------
+Url:    https://slack.com
+StatusCode:     200
+Length: 96.92kB
+Fetch timei:    0.336s
+---------------
+Command time: 0.336s
+```
--- cmd/fetch.go
+++ cmd/fetch.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 	"net/http/cookiejar"
-	"os"
 	"sync"
+	"time"
 
 	"github.com/docker/go-units"
 	"github.com/spf13/cobra"
@@ -22,53 +22,87 @@ var fetchCmd = &cobra.Command{
 	Run:   fetch,
 }
 
-type response struct {
-	Code int
-	Body string
+type fetchResult struct {
+	Code      int
+	Url, Body string
+	time      float64
 }
 
 func init() {
 	rootCmd.AddCommand(fetchCmd)
+	fetchCmd.PersistentFlags().Bool("use-channels", false, "Enable use of channels in fetch go routines")
+	fetchCmd.PersistentFlags().StringSlice("url", []string{}, "Slice of Urls to fetch")
 }
 
 func fetch(cmd *cobra.Command, args []string) {
-	urls := os.Args[2:]
-	if len(urls) < 1 {
-		log.Fatal("at least one url is expected as argument")
+	start := time.Now()
+	urls, err := cmd.PersistentFlags().GetStringSlice("url")
+	if err != nil || len(urls) < 1 {
+		log.Fatalf("error reading url flag: %v", err)
 	}
-	wg := sync.WaitGroup{}
-	options := cookiejar.Options{PublicSuffixList: publicsuffix.List}
-	jar, err := cookiejar.New(&options)
+	useChannels, err := cmd.PersistentFlags().GetBool("use-channels")
+	if err != nil {
+		log.Printf("error reading use-channels flag: %v", err)
+		useChannels = false
+	}
+
+	// create client wth cookie jar
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
 		log.Fatalf("Got error while creating cookie jar %s", err.Error())
 	}
 	client := http.Client{Jar: jar}
-	log.Printf("URLs to fetch: %v\n", urls)
-	for _, url := range urls {
-		wg.Add(1)
-		go fetchUrl(&client, url, &wg)
+
+	fmt.Printf("URLs to fetch: %v\n", urls)
+	fmt.Print("---------------\n")
+
+	if useChannels {
+		ch := make(chan fetchResult)
+		defer close(ch)
+		for _, url := range urls {
+			go fetchUrl(&client, url, nil, ch)
+		}
+		for _, _ = range urls {
+			res := <-ch
+			fmt.Printf("Url:\t%s\n", res.Url)
+			fmt.Printf("StatusCode:\t%d\n", res.Code)
+			fmt.Printf("Length:\t%s\n", units.HumanSize(float64(len(res.Body))))
+			fmt.Printf("Fetch timei:\t%.3fs\n", res.time)
+			fmt.Print("---------------\n")
+		}
+	} else {
+		wg := sync.WaitGroup{}
+		for _, url := range urls {
+			wg.Add(1)
+			go fetchUrl(&client, url, &wg, nil)
+		}
+		wg.Wait()
 	}
-	wg.Wait()
+	fmt.Printf("Command time: %.3fs\n", time.Since(start).Seconds())
 }
 
-func fetchUrl(c *http.Client, url string, wg *sync.WaitGroup) {
+func fetchUrl(c *http.Client, url string, wg *sync.WaitGroup, ch chan<- fetchResult) {
+	start := time.Now()
 	res, err := c.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	resp := response{Code: res.StatusCode}
+	resp := fetchResult{Code: res.StatusCode, Url: url}
 	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = res.Body.Close()
 	resp.Body = string(b)
-	fmt.Printf("Url:\t%s\n", url)
-	fmt.Printf("StatusCode:\t%d\n", resp.Code)
-	fmt.Printf("Length:\t%s\n", units.HumanSize(float64(len(resp.Body))))
-	fmt.Print("---------------\n")
-	wg.Done()
+	resp.time = time.Since(start).Seconds()
+	if ch != nil {
+		ch <- resp
+	} else if wg != nil {
+		fmt.Printf("Url:\t%s\n", url)
+		fmt.Printf("StatusCode:\t%d\n", resp.Code)
+		fmt.Printf("Length:\t%s\n", units.HumanSize(float64(len(resp.Body))))
+		fmt.Printf("Fetch time:\t%.3fs\n", time.Since(start).Seconds())
+		fmt.Print("---------------\n")
+		wg.Done()
+	}
 }
